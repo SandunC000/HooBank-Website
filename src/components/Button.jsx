c91d2cb67343f1db663681a6ac07db0acc06d0fc
SandunC000 <sanduncooray000@gmail.com>
2024-02-07 15:24:23 +0530

    final project

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <button type='button' className='rounded-full mt-5 py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary-outline-none '>
+      Get Started
+    </button>
+  )
+}
+
+export default Button
